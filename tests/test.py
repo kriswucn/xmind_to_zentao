b2a9f4ab56d11fbeab81674f213d12c8e2cb2f1d
kris <kriswucn@hotmail.com>
2020-04-10 16:51:38 +0800

    aa

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 from utils.xmind_utils import XmindUtils
 
-path = r'E:\PythonProjects\xmind_to_zentao\tests\dcc.xmind'
+# path = r'E:\01-智慧燃气管理平台\测试202004xx\采集系统v1.2测试用例.xmind'
+path = r'F:\py_projects\xmind_to_zentao\tests\dcc.xmind'
 xu = XmindUtils(path)
 roots = xu.roots
 
@@ -8,7 +9,17 @@ r = roots[0]
 
 # print(r.getData())
 
-xu.parse(r)
+xu.parse()
 
-for t in xu.test_cases:
-    print(t.getData())
+# for t in xu.test_cases:
+#     print(t.getData())
+
+nodes = xu.test_nodes
+
+for n in nodes:
+    xu.node_to_case(n)
+
+cs = xu.test_cases
+
+for c in cs:
+    print(c)
--- utils/xmind_utils.py
+++ utils/xmind_utils.py
@@ -1,12 +1,12 @@
 # coding:utf-8
 import xmind
-from xmind.core.markerref import MarkerRefElement
 import os
 
 
 class XmindUtils(object):
     def __init__(self, path):
         self._roots = []
+        self._test_nodes = []
         self._test_cases = []
 
         if not os.path.exists(path):
@@ -23,13 +23,54 @@ class XmindUtils(object):
         return self._roots
 
     @property
+    def test_nodes(self):
+        return self._test_nodes
+
+    @property
     def test_cases(self):
         return self._test_cases
+
+    # 测试节点to测试用例
+    def node_to_case(self, node):
+        if node is None:
+            return
+        test_case_dict = {}
+        # the node is test node
+        node_dict = node.getData()
+        test_case_dict.update({'case': node_dict.get('title')})
+        # print(node_dict.get('title'))
+        # step nodes
+        step_nodes = node.getSubTopics()
+        # 遍历步骤
+        # 步骤编号
+        step_num = 1
+
+        # 存放步骤和期望
+        step_str = ''
+        expecting_str = ''
+        for step in step_nodes:
+            step_dict = step.getData()
+            step_str += step_dict.get('title') + '\r\n'
+            # expecting为预期
+            # 只取一个只节点
+            exps = step.getSubTopics()
+            # 有期望的情况
+            if len(exps) > 0:
+                expecting_node = exps[0]
+                expecting_dict = expecting_node.getData()
+                expecting_str += expecting_dict.get('title') + '\r\n'
+
+            # 没有期望的情况
+            else:
+                expecting_str += '\r\n'
+        test_case_dict.update({'step': step_str.rstrip('\r\n'), 'expecting': expecting_str.rstrip('\r\n')})
+
+        self._test_cases.append(test_case_dict)
 
     @staticmethod
     def is_test_case(topic):
         if topic is None:
-            raise TypeError
+            raise Exception
 
         topic_dict = topic.getData()
         markers = topic_dict.get('markers')
@@ -41,17 +82,17 @@ class XmindUtils(object):
         return False
 
     # 解析并添加测试用例
-    def parse(self, topics):
+    def parse(self):
         for root in self._roots:
             if root.getSubTopics() is None:
                 continue
-
-            self.save_test_cases(root.getSubTopics())
+            module_prefix = root.getData().get('title')
+            self.save_test_nodes(root.getSubTopics(), module_prefix)
 
     # 添加测试用例
-    def save_test_cases(self, topics):
+    def save_test_nodes(self, topics):
         for t in topics:
             if self.is_test_case(t):
-                self._test_cases.append(t)
+                self._test_nodes.append(t)
             else:
-                self.save_test_cases(t.getSubTopics())
+                self.save_test_nodes(t.getSubTopics())
